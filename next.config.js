9e7950938ffc51c621f5575b936e3ccecfe4626a
Thea Lanherne <thea.lanherne@movegb.com>
2018-12-29 19:00:30 +1300

    Use now for deployments

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
-const withTypescript = require('@zeit/next-typescript');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const env =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === env.PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
 
-    return config;
-  },
-});
+  const withTypescript = require('@zeit/next-typescript');
+  return withTypescript();
+};

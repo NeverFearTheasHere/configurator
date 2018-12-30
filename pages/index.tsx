2fd3243b88a28f03aefe54756f76d248274d2a4c
Thea Lanherne <thea.lanherne@movegb.com>
2018-12-30 17:05:01 +1300

    Add material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,61 @@
 import React from 'react';
+import { withStyles, Theme } from '@material-ui/core/styles';
+import { ClassNameMap } from '@material-ui/core/styles/withStyles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ManufacturerSelect from '../src/components/ManufacturerSelect';
 
-export default () => <div>Welcome to the configurator</div>;
+type Props = {
+  classes: Partial<ClassNameMap>;
+};
+
+const styles = (theme: Theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
+
+const IndexPage: React.SFC<Props> = ({ classes }) => (
+  <div className={classes.root}>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Manufacturer</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <ManufacturerSelect />
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Category</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>Category select will go here</Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>MBS [kN]</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>Something will go here</Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Diameter Range [mm]</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>Something will go here</Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  </div>
+);
+
+export default withStyles(styles)(IndexPage);
---
+++ src/components/ManufacturerSelect.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+type Props = {};
+
+const ManufacturerSelect: React.SFC<Props> = () => (
+  <Typography>Manufacturer multi-select will go here</Typography>
+);
+
+export default ManufacturerSelect;
